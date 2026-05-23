c611086a9cd9a124226352c89785ddbbffb88e41
Harsh Dubey <69108118+harshdubs@users.noreply.github.com>
2026-05-23 09:56:20 +0000

    Did a few UI changes and added download buttons for resume and CV

--- app.py
+++ app.py
@@ -6,23 +6,35 @@ def process( file_path, job_description):
     "resume_path": file_path ,
     "job_description": job_description
     })
+    with open("updated_resume.txt", "w") as f:
+        f.write(result["updates"])
 
-    return result["updates"], result["cover_letter"], result["compatibility"]
+    with open("cover_letter.txt", "w") as f:
+        f.write(result["cover_letter"])
+        
+    return result["updates"], result["cover_letter"], result["compatibility"], "updated_resume.txt", "cover_letter.txt"
 
 with gr.Blocks(title="Job Application Agent") as demo:
     gr.Markdown("Job Application Agent")
-    resume =  gr.File(file_types=[".pdf"])
-    status = gr.Markdown("Upload a resume in pdf format")
-    job_description = gr.Textbox(placeholder="Paste Job Description!")
-    resume_updates = gr.Textbox()
-    cv = gr.Textbox()
-    compatibility_review = gr.Textbox()
+    resume =  gr.File(file_types=[".pdf"], label="Upload Resume (PDF)")
+    job_description = gr.Textbox(label="Job Description", lines=10)
+    
+    
+    with gr.Row():
+        resume_updates = gr.Textbox(label="Updated Resume", lines=10, scale=4)
+        resume_txt = gr.File(label="Download", scale=1)
+
+    with gr.Row():
+        cv = gr.Textbox(label="Cover Letter", lines=10, scale=4)
+        cv_txt = gr.File(label="Download CV", scale=1)
+    
+    compatibility_review = gr.Textbox(label="Job Compatibility", lines=10)
     start_button = gr.Button("Start!")
 
     start_button.click(
     fn=process,
     inputs=[resume,job_description],
-    outputs=[resume_updates, cv, compatibility_review]
+    outputs=[resume_updates, cv, compatibility_review, resume_txt,cv_txt]
     )
 
 if __name__ == "__main__":
